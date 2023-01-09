3739dbeca2a6d090b6938369fd30f498a7fe95f1
Kevin Torruco <Kevin_Torruco@jabil.com>
2023-01-09 16:54:25 -0600

    [ADD] ScUserTest

--- Modules/TE_TestProductivity/ScUsers.cs
+++ Modules/TE_TestProductivity/ScUsers.cs
@@ -1,16 +1,53 @@
 using TodoApi;
-
+using TodoApi.Infrastructure.Persistance;
+ using TodoApi.Infrastructure;
+using TodoApi.Controllers.Common;
+using TodoApi.Aplication;
+using System;
+using System.Collections.Generic;
 namespace TestWebApi;
 
-public class ScUsers
+public class ScUsersTest
 {
-     [Fact]
+
+   
+      private static TE_TestProductivityContext _context= new TE_TestProductivityContext();
+
+        private static IGenericRepository<TodoApi.Infrastructure.Persistance.ScUser> _repository = new GenericRepository<TodoApi.Infrastructure.Persistance.ScUser>(_context);
+
+         private readonly IGenericService <TodoApi.Infrastructure.Persistance.ScUser> _service = new GenericService<TodoApi.Infrastructure.Persistance.ScUser>(_repository);
+    
+    [Fact]
     public void getAlll()
     {
-        IGenericRepository<ScUser> _repository = new GenericRepository<ScUser>();
-       
-            var hola =  _repository.GetAll();
         
-        
+        List<ScUser> resultList = _service.GetAll().Result.ToList();
+           
+    }
+    [Theory]
+    [InlineData(1)]
+    [InlineData(4)]
+    public void getById(int id)
+    {
+      
+     ScUser result = _service.Get(id).Result;
+    }
+    [Fact]
+    public void create()
+    {
+        ScUser user = new ScUser();
+        user.FkmesUserId = 1;
+        user.EmployeeNumer = 1;
+        user.Ntuser = "test";
+        user.FirstName = "test";
+        user.LastName = "test";
+        user.SecondLastName = "test";
+        user.Email = "test";
+        user.FkroleId = 1;
+        user.Available = true;        
+        user.CreatedAt = DateTime.Now;
+        user.UpdatedAt = DateTime.Now;    
+        user.Available = true;
+        ScUser result = _service.Create(user).Result;
     }
 }
