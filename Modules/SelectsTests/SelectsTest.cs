3117f39a8da19c2e3ce9d44137a807345e311956
Kevin Torruco <Kevin_Torruco@jabil.com>
2023-07-10 17:15:49 -0600

    [add] GetManufacturingAreaTest

--- Modules/SelectsTests/SelectsTest.cs
+++ Modules/SelectsTests/SelectsTest.cs
@@ -1,61 +1,196 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Data;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 using Xunit;
 using FilesApi.Domain.Entities;
-using FilesApi.Infrastructure.Persistance;
 using FilesApi.Infrastructure.Persistance.DropDownsRepo;
+using FilesApi.Application.ProductivityRepository;
+using FilesApi.WebApi.Controllers.Services;
+using FilesApi.Application.DropDownsRepository;
 
 namespace TestWebApi
 {
+
     public class SelectsTest
     {
-
-        private readonly IDbConnect _dbConnectA;
-
-        private DropDownsRepo _dropDownRepo;
-
-        public SelectsTest()
-        {
-
-             // Configurar el servicio IDbConnectA en el contenedor de servicios
-            var services = new ServiceCollection();
-            services.AddScoped<IDbConnect, DbConnectA>();
-            
-            // Construir el proveedor de servicios
-            var serviceProvider = services.BuildServiceProvider();
-
-            // Obtener una instancia de IDbConnectA desde el proveedor de servicios
-            _dbConnectA = serviceProvider.GetRequiredService<IDbConnect>();      
+     
 
 
-               _dropDownRepo = new DropDownsRepo(_dbConnectA);
-      
-        }
-        [Fact]
-        public async Task BuildingSelectsTest()
-        {
-            Assert.True(true);
-
-            DropDown[] buildingSelect = new DropDown[]{
-               new DropDown  { id ="1",
-                    text="Prueba"                     
-                },
-                new DropDown {
-                    id ="2",
-                    text="Prueba2"
-
-                }
-            };
-            string jsonData = JsonConvert.SerializeObject(buildingSelect);  
-            Console.Write(jsonData);
-
-        }
+        private readonly DropDownsRepo _infraRepo;
 
        
 
+       private readonly IDbConnect _dbConnectA;
+
+       private readonly DropDownsRepository _appRepository;
+
+       private readonly DropDownsService _service;
+
+
+        public SelectsTest()
+        {
+              // Configurar el servicio IDbConnectA en el contenedor de servicios
+            var services = new ServiceCollection();
+            services.AddScoped<IDbConnect, DbConnectA>();
+
+              // Construir el proveedor de servicios
+            var serviceProvider = services.BuildServiceProvider();
+
+            // Obtener una instancia de IDbConnectA desde el proveedor de servicios
+            _dbConnectA = serviceProvider.GetRequiredService<IDbConnect>();
+
+            _infraRepo = new DropDownsRepo(_dbConnectA);
+
+
+            _appRepository = new DropDownsRepository(_infraRepo);
+
+            _service = new DropDownsService(_appRepository);
+           
+        }
+
+        [Fact]
+        public async Task GetBuildingsTest()
+        {
+            // Act
+            List<DropDown> buildingSelect = await _service.GetBuildings();
+
+            // Assert
+            Assert.NotNull(buildingSelect);
+            Assert.NotEmpty(buildingSelect);
+            //Assert.Equal(2, buildingSelect.Count);
+
+            foreach (var item in buildingSelect)
+            {
+                Assert.False(string.IsNullOrEmpty(item.id));
+                Assert.False(string.IsNullOrEmpty(item.text));
+            }
+        }
+
+        [Fact]
+        public async Task GetManufacturingAreaTest()
+        {
+            
+            DateTime fecha = new DateTime(2023,7,5);
+            int Shift = 1;
+            int customerID = 5;
+            string testRouteStep = "ALL";
+            // Act
+            //List<DropDown> selects = await _infraRepo.up_GetTestManufacturingArea_TestDashd_V2(fecha,Shift,customerID,testRouteStep);
+            List<DropDown> selects = await _service.GetTestManufacturingArea(fecha,Shift,customerID,testRouteStep);
+
+            // Assert
+            Assert.NotNull(selects);
+            Assert.NotEmpty(selects);
+            //Assert.Equal(2, selects.Count);
+
+            foreach (var item in selects)
+            {
+                Assert.False(string.IsNullOrEmpty(item.id));
+                Assert.False(string.IsNullOrEmpty(item.text));
+            }
+        }
+
+        [Fact]
+        public async Task GetCustomersTest()
+        {
+            int idBuilding = 0;
+            // Act
+            List<DropDown> selects = await _service.GetCustomersByBuilding(idBuilding);
+
+            // Assert
+            Assert.NotNull(selects);
+            Assert.NotEmpty(selects);
+            //Assert.Equal(2, selects.Count);
+
+            foreach (var item in selects)
+            {
+                Assert.False(string.IsNullOrEmpty(item.id));
+                Assert.False(string.IsNullOrEmpty(item.text));
+            }
+        }
+         [Fact]
+        public async Task GetAreasTest()
+        {
+           
+            // Act
+            List<DropDown> selects = await _service.GetAreas();
+
+            // Assert
+            Assert.NotNull(selects);
+            Assert.NotEmpty(selects);
+            //Assert.Equal(2, selects.Count);
+
+            foreach (var item in selects)
+            {
+                Assert.False(string.IsNullOrEmpty(item.id));
+                Assert.False(string.IsNullOrEmpty(item.text));
+            }
+        }
+
+        [Fact]
+        public async Task GetPcbaFamiliesByCustomerTest()
+        {
+            int idCustomer=5;//Aclara
+           
+            // Act
+            //List<DropDown> selects = await _infraRepo.Up_getPCBAFamiliesByCustomer(idCustomer);
+            List<DropDown> selects = await _service.GetPCBAFamiliesByCustomer(idCustomer);
+
+            // Assert
+            Assert.NotNull(selects);
+            Assert.NotEmpty(selects);
+            //Assert.Equal(2, selects.Count);
+
+            foreach (var item in selects)
+            {
+                Assert.False(string.IsNullOrEmpty(item.id));
+                Assert.False(string.IsNullOrEmpty(item.text));
+            }
+        }
+
+        [Fact]
+        public async Task GetPositionsTest()
+        {
+           
+            // Act
+              //List<DropDown> selects = await _infraRepo.Up_GetPositionTypes();
+            List<DropDown> selects = await _service.GetPositionTypes();
+
+            // Assert
+            Assert.NotNull(selects);
+            Assert.NotEmpty(selects);
+            //Assert.Equal(2, selects.Count);
+
+            foreach (var item in selects)
+            {
+                Assert.False(string.IsNullOrEmpty(item.id));
+                Assert.False(string.IsNullOrEmpty(item.text));
+            }
+        }
+
+        [Fact]    
+        public async Task GetShiftsTest()
+        {
+           
+            // Act
+              List<DropDown> selects = await _infraRepo.Up_GetShiftsAvailable();
+            //List<DropDown> selects = await _service.GetPositionTypes();
+
+            // Assert
+            Assert.NotNull(selects);
+            Assert.NotEmpty(selects);
+            //Assert.Equal(2, selects.Count);
+
+            foreach (var item in selects)
+            {
+                Assert.False(string.IsNullOrEmpty(item.id));
+                Assert.False(string.IsNullOrEmpty(item.text));
+            }
+        }
+
+        
     }
 }
--- Modules/databaseTest/DatabaseTests.cs
+++ Modules/databaseTest/DatabaseTests.cs
@@ -15,7 +15,7 @@ namespace TestWebApi
    
     public class DatabaseTests
     {
-         private readonly IDbConnect _dbConnectA;
+         private  IDbConnect _dbConnectA;
 
           private ProductivityRepo _repository;
 
@@ -36,9 +36,9 @@ namespace TestWebApi
 
              _storeProceduresRepo = new StoreProceduresRepo(_dbConnectA);
 
-             _repository = new ProductivityRepo(_dbConnectA);
+             _repository = new ProductivityRepo(_storeProceduresRepo);
 
-             this._service = new ProductivityServices(_dbConnectA);
+             this._service = new ProductivityServices(_repository);
         }
         private const string ConnectionTE_MESApi = "Data Source=MXGDLM0OTSQLV2A;Initial Catalog=TE_MESApi;Persist Security Info=True;User ID=EngSWUser;Password=EnGswUs3r19!;TrustServerCertificate=true;";
         
@@ -56,7 +56,48 @@ namespace TestWebApi
             
             var items = await _service.GetDebugRepairEmployeesHour(new DateTime(2023,6,1),1,0,81,0,0,0);                
 
-            List<DebugRepairEmployeesHour> result = items;
+            List<DebugRepairEmployees> result = items;
+
+            // Convertir el objeto a JSON utilizando Newtonsoft.Json
+            string jsonData = JsonConvert.SerializeObject(result);                    
+                                   
+        }
+
+        [Fact]
+        public async Task up_GetDebugAndRepairByDayAsync()
+        {
+            DateTime pDatetime = new DateTime(2023, 5, 17);
+            //int pShift = 2; // Cambiar el valor del shift según sea necesario
+            int pBuildingID = 0;
+            int pCustomerID = 29;
+            int pAreaID = 0;
+            int pPCBAFamilyID = 0;
+            int PositionType = 0;
+            
+          //  var items =  _storeProceduresRepo.Up_GetDebugAndRepairByDay(pDatetime,pBuildingID,pCustomerID,pAreaID,pPCBAFamilyID,PositionType);                
+
+            var items = await  _service.GetDebugRepairEmployeesDay(pDatetime,pBuildingID,pCustomerID,pAreaID,pPCBAFamilyID,PositionType);                
+
+            List<DebugRepairEmployees> result = items;
+
+            // Convertir el objeto a JSON utilizando Newtonsoft.Json
+            string jsonData = JsonConvert.SerializeObject(result);                    
+                                   
+        }
+
+        [Fact]
+        public async Task up_GetDebugAndRepairByCustomer()
+        {
+            DateTime pDatetime = new DateTime(2023, 5, 17);
+            //int pShift = 2; // Cambiar el valor del shift según sea necesario
+            int pBuildingID = 0;
+            int debugRepairType=1;
+            
+            var items =  _storeProceduresRepo.up_GetDebugAndRepairByCustomer(pDatetime,pBuildingID,debugRepairType);                
+
+           // var items = await  _service.GetDebugRepairEmployeesDay(pDatetime,pBuildingID,pCustomerID,pAreaID,pPCBAFamilyID,PositionType);                
+
+            List<DebugRepairCustomers> result = items;
 
             // Convertir el objeto a JSON utilizando Newtonsoft.Json
             string jsonData = JsonConvert.SerializeObject(result);                    
